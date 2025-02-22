f86dc9f8be7483ca2706e58403b7b5e25f103c8a
Yuvasri0304 <yuvasrip2022@gmail.com>
2025-02-22 17:33:29 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,224 @@
+export const navItems = [
+    { name: "About", link: "#about" },
+    { name: "Resume", link: "https://drive.google.com/file/d/1A7_6SnPNmUIvqwNMwq8q1YRNKgeHFinP/view?usp=sharing" },
+    { name: "Projects", link: "#projects" },
+    //{ name: "Testimonials", link: "#testimonials" },
+    
+    { name: "Contact", link: "#contact" },
+  ];
+  
+  export const gridItems = [
+    {
+      id: 1,
+      title: "I prioritize client collaboration, fostering open communication",
+      description: "",
+      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+      imgClassName: "w-full h-full",
+      titleClassName: "justify-end",
+      img: "/b1.svg",
+      spareImg: "",
+    },
+    {
+      id: 2,
+      title: "I'm very flexible with time zone communications",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 min-h-[40vh] select-none",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 3,
+      title: "My Resume",
+      description: "Are you hiring? Check out ",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-2 flex",
+      imgClassName: "",
+      titleClassName: "justify-end",
+      img: "",
+      spareImg: "",
+    },
+    {
+      id: 4,
+      title: "Tech enthusiast with a passion for development.",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-start",
+      img: "/grid.svg",
+      spareImg: "/b4.svg",
+    },
+  
+    {
+      id: 5,
+      title: "Clean Code and Modern Design",
+      description: "Passionate about transforming ideas into reality.",
+      className: "md:col-span-3 md:row-span-2",
+      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+      titleClassName: "justify-center md:justify-start lg:justify-center",
+      img: "/b5.svg",
+      spareImg: "/grid.svg",
+    },
+    {
+      id: 6,
+      title: "Do you want to start a project together?",
+      description: "",
+      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+      imgClassName: "",
+      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+      img: "",
+      spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+        {
+      id: 1,
+      title: "AgriSoft",
+      des: "AgriSoft is an ERP-CRM app build for Agritech Customized for their requirements",
+      img: "/h.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png"],
+      link: "https://github.com/Yuvasri0304/AgriSoft",
+    },
+    {
+      id: 2,
+      title: "Gym CRM",
+      des: "We made a customized management app for Chamber Fitness , Permanagalam , with attendance tracking and reports , membership management and automated notifications , Expense and income management ,  ",
+      img: "chamber.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png","/mui.svg"],
+      link: "https://github.com/Yuvasri0304/GYM-CRM",
+      
+    },
+    {
+      id: 3,
+      title: "TeamSync AI",
+      des: "Responsive Ai powered chat app with project collaborations, file sharing, Group chat and much more",
+      img: "/chat.png",
+      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+      link: "https://github.com/Yuvasri0304/TeamSync-AI",
+    },
+  
+    {
+      id: 4,
+      title: "Eventura",
+      des: "It is a digital marketing and event planning web application , used by digital marketing agencies to scedule task to creators and editors , manage billing and event scheduling attendance tracking of staff etc",
+      img: "/digi.png",
+      iconLists: ["/science.png",  "/tail.svg", "/supabase.png","/mui.svg"],
+      link: "https://github.com/Yuvasri0304/Eventura",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+    {
+      quote:
+        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      name: "Michael Johnson",
+      title: "Director of AlphaStream Technologies",
+    },
+  ];
+  
+  export const companies = [
+    {
+      id: 1,
+      name: "cloudinary",
+      img: "/cloud.svg",
+      nameImg: "/cloudName.svg",
+    },
+    {
+      id: 2,
+      name: "appwrite",
+      img: "/app.svg",
+      nameImg: "/appName.svg",
+    },
+    {
+      id: 3,
+      name: "HOSTINGER",
+      img: "/host.svg",
+      nameImg: "/hostName.svg",
+    },
+    {
+      id: 4,
+      name: "stream",
+      img: "/s.svg",
+      nameImg: "/streamName.svg",
+    },
+    {
+      id: 5,
+      name: "docker.",
+      img: "/dock.svg",
+      nameImg: "/dockerName.svg",
+    },
+  ];
+  
+  export const workExperience = [
+    {
+      id: 1,
+      title: "Full Stack Developer Intern at DSC Solutions",
+      desc: "Worked on building a CRM system for both small and large-scale businesses, enhancing customer relationship management and optimizing business workflows",
+      className: "md:col-span-2",
+      thumbnail: "/exp1.svg",
+    },
+    {
+      id: 2,
+      title: "SDE Intern at Ajashia Technologies",
+      desc: "Developing and enhancing cloud-based services for small and large-scale businesses, focusing on improving scalability, performance, and user interface",
+      thumbnail: "/exp2.svg",
+    },
+    {
+      id: 3,
+      title: "Freelance Project",
+      desc: "Developed a AR based web application for a client to present his machines AR gltf files to get funding for his big project",
+      className: "md:col-span-2",
+      thumbnail: "/exp3.svg",
+    },
+    {
+      id: 4,
+      title: "Team Lead",
+      desc: "Led the team of developers at whitebranding for multiple projects for a period of 6 months",
+      className: "md:col-span-2",
+      thumbnail: "/exp4.svg",
+    },
+  ];
+  
+  
+  export const socialMedia = [
+    {
+      id: 1,
+      img: "/git.svg",
+      link: "https://github.com/Yuvasri0304",
+    },
+    {
+      id: 2,
+      img: "/leetcode.svg",
+      link: "https://leetcode.com/u/YUVASRI-04_30/",
+    },
+    {
+      id: 3,
+      img: "/link.svg",
+      link: "https://www.linkedin.com/in/yuvasrip",
+    },
+  ];
